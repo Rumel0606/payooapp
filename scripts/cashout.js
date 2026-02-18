7c3f8f3e3f0762122ae1f97997f35c85eb2b47bd
MD RUMEL AHMED <rumelrumel0987654321@gmail.com>
2026-02-18 12:24:32 +0600

    duhor time

---
+++ scripts/cashout.js
@@ -0,0 +1,10 @@
+document.getElementById("cashout-btn").addEventListener('click',function(){
+    console.log("clicked");
+    // 1.get the agent number and validate
+    // 2.get the amount,validate,convert to number 
+    // 3.get the ureent balance,valudate,convert to number 
+    // 4. calculate new balance 
+    // 5.get the pin and verify
+    // 6.true:: show an alert >set new balance 
+    // 6.true:: show an eror alert >retrun
+})
